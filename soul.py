32cce768b4d46ffc5dced5b8e99449a3a0825de1
kim0218w <152501041+kim0218w@users.noreply.github.com>
2026-08-31 21:10:42 +0900

    Integrate GPIO control for motor operation

--- soul.py
+++ soul.py
@@ -1,23 +1,56 @@
-import cv2
+import gpiod
+import gpiod.line as gpiod_line
+import time
 
-image = cv2.imread('test.jpg')
+DIR = 19
+PULSE = 20
+ENA = 16
 
-if image is not None:
-    # 1. BGR 컬러를 흑백(Grayscale)으로 변환
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+chip = gpiod.Chip("/dev/gpiochip4")
 
-    # 2. 가우시안 블러 (이미지를 부드럽게/흐리게 처리하여 노이즈 제거)
-    # (5, 5)는 커널 크기로, 홀수(3, 5, 7 등)로 설정합니다.
-    blurred_image = cv2.GaussianBlur(image, (5, 5), 0)
+settings = gpiod.LineSettings(
+    direction=gpiod_line.Direction.OUTPUT
+)
 
-    # 3. Canny 알고리즘을 이용한 외각선(Edge) 검출
-    # 임계값(Threshold) 100과 200 사이의 경계선을 찾아냅니다.
-    edges = cv2.Canny(gray_image, 100, 200)
+request = chip.request_lines(
+    consumer="motor_test",
+    config={
+        (DIR, PULSE, ENA): settings
+    }
+)
 
-    # 결과 보기
-    cv2.imshow('Gray', gray_image)
-    cv2.imshow('Blurred', blurred_image)
-    cv2.imshow('Edges', edges)
+# 초기값
+request.set_values({
+    DIR: gpiod_line.Value.INACTIVE,
+    PULSE: gpiod_line.Value.ACTIVE,
+    ENA: gpiod_line.Value.ACTIVE
+})
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+time.sleep(1)
+
+# ENA LOW = Enable
+request.set_value(ENA, gpiod_line.Value.INACTIVE)
+
+time.sleep(0.5)
+
+# 방향
+request.set_value(DIR, gpiod_line.Value.INACTIVE)
+
+time.sleep(0.5)
+
+print("START")
+
+for i in range(400):
+
+    request.set_value(PULSE, gpiod_line.Value.INACTIVE)
+    time.sleep(0.005)
+
+    request.set_value(PULSE, gpiod_line.Value.ACTIVE)
+    time.sleep(0.005)
+
+print("DONE")
+
+request.set_value(ENA, gpiod_line.Value.ACTIVE)
+
+request.release()
+chip.close()
